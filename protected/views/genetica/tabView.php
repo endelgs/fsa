6605b0a3b9b14c358be3ac9d2e746abcaba2af23
joaotonussi <joaotonussi@gmail.com>
2012-09-26 14:21:45 -0700

    tab view genetica

--- protected/views/genetica/tabView.php
+++ protected/views/genetica/tabView.php
@@ -13,6 +13,32 @@ $this->menu=array(
 	array('label'=>'Remover Triagem', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Você tem certeza que quer remover este? Triagem')),
 	array('label'=>'Gerenciar Triagem', 'url'=>array('admin')),
 );
+
+$model->homozigoto_normal=($model->homozigoto_normal == 'true')?'Sim':'Não';
+$model->homozigoto_mutante=($model->homozigoto_mutante == 'true')?'Sim':'Não';
+$model->heterozigoto=($model->heterozigoto == 'true')?'Sim':'Não';
+
+$model->del_gjb6_d13s1830 = ($model->del_gjb6_d13s1830 == 'true')?'Sim':'Não';
+$model->del_gjb6_d13s1854 = ($model->del_gjb6_d13s1854 == 'true')?'Sim':'Não';
+$model->del_3 = ($model->del_3 == 'true')?'Sim':'Não';
+$model->del_4 = ($model->del_4 == 'true')?'Sim':'Não';
+$model->del_5 = ($model->del_5 == 'true')?'Sim':'Não';
+
+$model->a1555g = ($model->a1555g == 'true')?'Sim':'Não';
+$model->c1494t = ($model->c1494t == 'true')?'Sim':'Não';
+$model->a827g = ($model->a827g == 'true')?'Sim':'Não';
+$model->a7445g = ($model->a7445g == 'true')?'Sim':'Não';
+$model->a7444g = ($model->a7444g == 'true')?'Sim':'Não';
+$model->a3243g = ($model->a3243g == 'true')?'Sim':'Não';
+
+$model->gjb2 = ($model->gjb2 == 'true')?'Sim':'Não';
+$model->gjb3 = ($model->gjb3 == 'true')?'Sim':'Não';
+$model->gjb6 = ($model->gjb6 == 'true')?'Sim':'Não';
+$model->slc26a4 = ($model->slc26a4 == 'true')?'Sim':'Não';
+$model->otof = ($model->otof == 'true')?'Sim':'Não';
+$model->mtrnr1 = ($model->mtrnr1 == 'true')?'Sim':'Não';
+$model->outros_genes = ($model->outros_genes == 'true')?'Sim':'Não';
+
 $model->homozigoto_normal=($model->homozigoto_normal == 'true')?'Sim':'Não';
 $model->homozigoto_mutante=($model->homozigoto_mutante == 'true')?'Sim':'Não';
 $model->heterozigoto=($model->heterozigoto == 'true')?'Sim':'Não';
@@ -37,6 +63,7 @@ $model->slc26a4 = ($model->slc26a4 == 'true')?'Sim':'Não';
 $model->otof = ($model->otof == 'true')?'Sim':'Não';
 $model->mtrnr1 = ($model->mtrnr1 == 'true')?'Sim':'Não';
 $model->outros_genes = ($model->outros_genes == 'true')?'Sim':'Não';
+
 ?>
 
 <style>
