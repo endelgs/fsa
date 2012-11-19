9db0061cd88166453d1c6ae1b332be5432ff2727
joaotonussi <joaotonussi@gmail.com>
2012-11-19 06:14:39 -0800

    db controle acesso e usuarios

--- sql/saudeaud_fcm.sql
+++ sql/saudeaud_fcm.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tempo de Geração: 17/11/2012 às 11h16min
+-- Tempo de Geração: 19/11/2012 às 12h13min
 -- Versão do Servidor: 5.5.28
 -- Versão do PHP: 5.3.10-1ubuntu3.4
 
@@ -1405,93 +1405,6 @@ INSERT INTO `servico_social` (`id`, `nome_mae`, `data_nascimento_mae`, `escolari
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `tbl_profiles`
---
-
-CREATE TABLE IF NOT EXISTS `tbl_profiles` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `lastname` varchar(50) NOT NULL DEFAULT '',
-  `firstname` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
-
---
--- Extraindo dados da tabela `tbl_profiles`
---
-
-INSERT INTO `tbl_profiles` (`user_id`, `lastname`, `firstname`) VALUES
-(1, 'Admin', 'Administrator'),
-(2, 'Demo', 'Demo');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tbl_profiles_fields`
---
-
-CREATE TABLE IF NOT EXISTS `tbl_profiles_fields` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `varname` varchar(50) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `field_type` varchar(50) NOT NULL,
-  `field_size` varchar(15) NOT NULL DEFAULT '0',
-  `field_size_min` varchar(15) NOT NULL DEFAULT '0',
-  `required` int(1) NOT NULL DEFAULT '0',
-  `match` varchar(255) NOT NULL DEFAULT '',
-  `range` varchar(255) NOT NULL DEFAULT '',
-  `error_message` varchar(255) NOT NULL DEFAULT '',
-  `other_validator` varchar(5000) NOT NULL DEFAULT '',
-  `default` varchar(255) NOT NULL DEFAULT '',
-  `widget` varchar(255) NOT NULL DEFAULT '',
-  `widgetparams` varchar(5000) NOT NULL DEFAULT '',
-  `position` int(3) NOT NULL DEFAULT '0',
-  `visible` int(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `varname` (`varname`,`widget`,`visible`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
-
---
--- Extraindo dados da tabela `tbl_profiles_fields`
---
-
-INSERT INTO `tbl_profiles_fields` (`id`, `varname`, `title`, `field_type`, `field_size`, `field_size_min`, `required`, `match`, `range`, `error_message`, `other_validator`, `default`, `widget`, `widgetparams`, `position`, `visible`) VALUES
-(1, 'lastname', 'Last Name', 'VARCHAR', '50', '3', 1, '', '', 'Incorrect Last Name (length between 3 and 50 characters).', '', '', '', '', 1, 3),
-(2, 'firstname', 'First Name', 'VARCHAR', '50', '3', 1, '', '', 'Incorrect First Name (length between 3 and 50 characters).', '', '', '', '', 0, 3);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tbl_users`
---
-
-CREATE TABLE IF NOT EXISTS `tbl_users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) NOT NULL,
-  `password` varchar(128) NOT NULL,
-  `email` varchar(128) NOT NULL,
-  `activkey` varchar(128) NOT NULL DEFAULT '',
-  `create_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `lastvisit_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `superuser` int(1) NOT NULL DEFAULT '0',
-  `status` int(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`),
-  KEY `status` (`status`),
-  KEY `superuser` (`superuser`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
-
---
--- Extraindo dados da tabela `tbl_users`
---
-
-INSERT INTO `tbl_users` (`id`, `username`, `password`, `email`, `activkey`, `create_at`, `lastvisit_at`, `superuser`, `status`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'webmaster@example.com', '9a24eff8c15a6a141ece27eb6947da0f', '2012-11-15 13:35:44', '2012-11-15 13:39:00', 1, 1),
-(2, 'demo', 'fe01ce2a7fbac8fafaed7c982a04e229', 'demo@example.com', '099f825543f7850cc038b90aaff39fac', '2012-11-15 13:35:44', '0000-00-00 00:00:00', 0, 1);
-
--- --------------------------------------------------------
-
---
 -- Estrutura da tabela `triagem`
 --
 
@@ -1612,6 +1525,32 @@ INSERT INTO `triagem` (`id`, `peso`, `apgar_1`, `apgar_5`, `apgar_10`, `apgar_15
 (53, 0, 0, 0, NULL, NULL, 0, 0, 'capurro', NULL, 'termo', 'aig', 'peate-a', 'passou', 'passou', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', 'true', '', '', 'true', 'true', 'true', 'true', 'true', '', 'true', 'true', 'true', 'true', '2012-11-14 14:33:26', 38),
 (54, 1, 1, 1, 1, 1, 1, 1, 'capurro', NULL, 'termo', 'aig', 'peate-a', 'passou', 'passou', 'true', 'true', 'true', 'false', 'true', 'false', 'true', 'false', 'true', 'false', 'true', 'false', 'true', 'false', 'true', 'false', '', 'true', 'false', 'false', 'false', 'false', 'false', 'asdadasd', 'asdasdasd', 'true', 'true', 'false', 'false', 'false', 'asdasdadasd', 'false', 'false', 'true', 'false', '2012-11-16 14:03:33', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_completo` varchar(300) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `roles` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Extraindo dados da tabela `user`
+--
+
+INSERT INTO `user` (`id`, `nome_completo`, `username`, `password`, `email`, `roles`) VALUES
+(1, 'Admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@teste.com', 'admin'),
+(2, 'Triagem', 'triagem', '6dcb01378179a086e279e999d91c4a14', 'triagem@teste.com', 'triagem'),
+(3, 'Serviço Social', 'servicosocial', '04f8dd33d9b1b24fc306d593b0172b9d', 'servicosocial@teste.com', 'servicosocial'),
+(5, 'Teste Genética', 'testegenetica', '4240721df02821d62d6e3b39f4617882', 'tgenetica@teste.com', 'genetica');
+
 --
 -- Restrições para as tabelas dumpadas
 --
@@ -1709,12 +1648,6 @@ ALTER TABLE `protese_prescricao`
 --
 ALTER TABLE `residentes_familia_servico_social`
   ADD CONSTRAINT `residentes_familia_servico_social_ibfk_1` FOREIGN KEY (`servico_social_r`) REFERENCES `servico_social` (`id`);
-
---
--- Restrições para a tabela `tbl_profiles`
---
-ALTER TABLE `tbl_profiles`
-  ADD CONSTRAINT `user_profile_id` FOREIGN KEY (`user_id`) REFERENCES `tbl_users` (`id`) ON DELETE CASCADE;
 
 --
 -- Restrições para a tabela `triagem`
